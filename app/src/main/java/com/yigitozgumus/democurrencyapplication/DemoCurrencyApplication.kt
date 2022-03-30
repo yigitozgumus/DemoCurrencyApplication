719a6ed5fd9da259c337638b4e9437f438c6aa35
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-03-30 23:04:35 +0300

    Enable Hilt on app module and connect to the network module

---
+++ app/src/main/java/com/yigitozgumus/democurrencyapplication/DemoCurrencyApplication.kt
@@ -0,0 +1,7 @@
+package com.yigitozgumus.democurrencyapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DemoCurrencyApplication : Application()
