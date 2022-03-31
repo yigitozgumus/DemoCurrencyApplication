3dfba5bb1f0ef0f17153fab09e73bbf01fe13781
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-03-31 19:32:03 +0300

    Add viewbinding support

--- app/src/main/java/com/yigitozgumus/democurrencyapplication/MainActivity.kt
+++ app/src/main/java/com/yigitozgumus/democurrencyapplication/MainActivity.kt
@@ -2,13 +2,23 @@ package com.yigitozgumus.democurrencyapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.yigitozgumus.democurrencyapplication.databinding.ActivityFragmentContainerBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityFragmentContainerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityFragmentContainerBinding.inflate(layoutInflater)
+        // add main screen
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
